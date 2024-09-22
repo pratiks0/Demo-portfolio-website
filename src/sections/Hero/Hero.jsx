02d10664d868d29246e05c5939647a24b3633371
Pratik Singh <pratik00888@gmail.com>
2024-09-22 19:33:30 +0530

    2nd

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,12 +1,44 @@
 import styles from './HeroStyles.module.css'
 import heroImg from '../../assets/hero-img.png'
+import themeIcon from '../../assets/sun.svg'
+import twitterIcon from '../../assets/twitter-light.svg'
+import githubIcon from '../../assets/github-light.svg'
+import linkedinIcon from '../../assets/linkedin-light.svg'
+import CV from '../../assets/cv.pdf'
 
 function Hero() {
   return (
-    <section id='hero'>
+    <section id='hero' className={styles.conatiner}>
         <div className={styles.colorModeContainer}>
             <img className={styles.hero} src={heroImg} alt="profile pic" />
             <img className={styles.colorMode} src={themeIcon} alt="color mode icon" />
+        </div>
+        <div className="styles.info">
+            <h1>
+                Pratik 
+                <br />
+                Singh
+            </h1>
+            <h2>Frontend Developer</h2>
+            <span>
+                <a href="https://twitter.com/" target='_blank'>
+                <img src={twitterIcon} alt="twitter icon" />
+                </a>
+                <a href="https://github.com/" target='_blank'>
+                <img src={githubIcon} alt="twitter icon" />
+                </a>
+                <a href="https://linkedin.com/" target='_blank'>
+                <img src={linkedinIcon} alt="twitter icon" />
+                </a>
+            </span>
+            <p>
+                I'm a frontend developer with a passion for building user-friendly and visually appealing applications.
+            </p>
+            <a href={CV}>
+                <button className='hover'>
+                    Resume
+                </button>
+            </a>
         </div>
     </section>
   )
