bd23c5c05b877806b0ddea035c52c99813e0e1a3
Pratik Singh <pratik00888@gmail.com>
2024-09-22 22:09:58 +0530

    3rd

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,17 +1,30 @@
 import styles from './HeroStyles.module.css'
 import heroImg from '../../assets/hero-img.png'
-import themeIcon from '../../assets/sun.svg'
-import twitterIcon from '../../assets/twitter-light.svg'
-import githubIcon from '../../assets/github-light.svg'
-import linkedinIcon from '../../assets/linkedin-light.svg'
+import sun from '../../assets/sun.svg'
+import moon from '../../assets/moon.svg'
+import twitterLight from '../../assets/twitter-light.svg'
+import twitterDark from '../../assets/twitter-dark.svg'
+import githubLight from '../../assets/github-light.svg'
+import githubDark from '../../assets/github-dark.svg'
+import linkedinLight from '../../assets/linkedin-light.svg'
+import linkedinDark from '../../assets/linkedin-dark.svg'
 import CV from '../../assets/cv.pdf'
+import { useTheme } from '../../common/ThemeContext'
 
 function Hero() {
+    const { theme, toggleTheme } = useTheme();
+
+    const themeIcon = theme == 'light' ? sun:moon;
+    const twitterIcon = theme == 'light' ? twitterLight:twitterDark;
+    const githubIcon = theme == 'light' ? githubLight:githubDark;
+    const linkedinIcon = theme == 'light' ? linkedinLight:linkedinDark;
+
+
   return (
     <section id='hero' className={styles.conatiner}>
         <div className={styles.colorModeContainer}>
             <img className={styles.hero} src={heroImg} alt="profile pic" />
-            <img className={styles.colorMode} src={themeIcon} alt="color mode icon" />
+            <img className={styles.colorMode} src={themeIcon} alt="color mode icon" onClick={toggleTheme} />
         </div>
         <div className="styles.info">
             <h1>
@@ -31,7 +44,7 @@ function Hero() {
                 <img src={linkedinIcon} alt="twitter icon" />
                 </a>
             </span>
-            <p>
+            <p className={styles.description}>
                 I'm a frontend developer with a passion for building user-friendly and visually appealing applications.
             </p>
             <a href={CV}>
