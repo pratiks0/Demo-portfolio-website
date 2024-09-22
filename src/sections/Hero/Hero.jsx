29aa0376b3d5a193c07a9a0f53c57d329e9b29b8
Pratik Singh <pratik00888@gmail.com>
2024-09-22 22:27:11 +0530

    3rd

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -34,13 +34,13 @@ function Hero() {
             </h1>
             <h2>Frontend Developer</h2>
             <span>
-                <a href="https://twitter.com/" target='_blank'>
+                <a href="https://x.com/prat1k_s" target='_blank'>
                 <img src={twitterIcon} alt="twitter icon" />
                 </a>
-                <a href="https://github.com/" target='_blank'>
+                <a href="https://github.com/pratiks0" target='_blank'>
                 <img src={githubIcon} alt="twitter icon" />
                 </a>
-                <a href="https://linkedin.com/" target='_blank'>
+                <a href="https://www.linkedin.com/in/pratik-singh-a20389314/" target='_blank'>
                 <img src={linkedinIcon} alt="twitter icon" />
                 </a>
             </span>
