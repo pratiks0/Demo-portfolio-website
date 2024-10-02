026cb36f9b52b9406c9b0d9868e5342885310fac
Pratik Singh <pratik00888@gmail.com>
2024-10-02 20:42:05 +0530

    final commit

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,7 +1,6 @@
 import styles from './ProjectStyles.module.css';
 import viberr from '../../assets/viberr-removebg-preview.png';
-import freshBurger from '../../assets/fresh-burger-removebg-preview.png';
-import hipsster from '../../assets/hipsster.png';
+import freshBurger from '../../assets/realogo-removebg-preview.png';
 import fitLift from '../../assets/fitlift-removebg-preview.png';
 import ProjectCard from '../../common/ProjectCard';
 
@@ -11,22 +10,19 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
+          src={freshBurger}
+          link="https://real-estate-website-mocha-eta.vercel.app/"
+          h3="Real Estate"
+        />
+        <ProjectCard
           src={viberr}
           link="https://endearing-marigold-97e1af.netlify.app/"
           h3="Sputifi"
-          p="Songs Streaming App"
-        />
-        <ProjectCard
-          src={freshBurger}
-          link="https://moonlit-axolotl-806740.netlify.app/"
-          h3="Weather Wise"
-          p="Weather App"
         />
         <ProjectCard
           src={fitLift}
           link="https://youtu.be/dQw4w9WgXcQ?si=mX8yj6vEcy9WOFNw"
           h3="FitLift"
-          p="Fitness App"
         />
       </div>
     </section>
